0a1f06df7a32a80832049c96cc1dae38a4817226
provashish95 <roytonmoy95@gmail.com>
2025-07-17 13:06:34 +0600

    Admin login & logout test

--- src/test/java/AdminLoginTestRunner.java
+++ src/test/java/AdminLoginTestRunner.java
@@ -1,11 +1,22 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AdminLoginTestRunner extends Setup {
-
 
     @Test(priority = 1, description = "Admin can login successfully with valid email and password")
     public void doLogin() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.adminLogin("admin@test.com", "admin123");
+
+        String txtHeaderActual = driver.findElement(By.tagName("h2")).getText();
+        String txtHeaderExpected = "Admin Dashboard";
+        Assert.assertEquals(txtHeaderActual, txtHeaderExpected);
+    }
+
+    @Test(priority = 1, description = "Admin can logout successfully")
+    public void logout() {
+        DashboardPage dashboardPage = new DashboardPage(driver);
+        dashboardPage.doLogout();
     }
 }
